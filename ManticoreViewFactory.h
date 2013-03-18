141158fae0c518df4a1590b0b9a6b73296b7853b
Richard Fung <richard@yetihq.com>
2013-03-18 13:12:23 -0700

    fixes to initial project

---
+++ ManticoreViewFactory.h
@@ -0,0 +1,14 @@
+
+//  MCViewFactory.h
+//  Manticore iOSViewFactory
+//
+//  Created by Richard Fung on 3/15/13.
+//  Copyright (c) 2013 Yeti LLC. All rights reserved.
+//
+
+#import "MCAppDelegate.h"
+#import "MCSectionViewController.h"
+#import "MCViewController.h"
+#import "MCViewFactory.h"
+#import "MCViewModel.h"
+#import "AppModelIntent.h"
